b2165a7df62c3d0e63b625a8ba8f06723c675167
Jyotsna Ramaswamy <jo.ramaswamy@gmail.com>
2020-06-23 01:25:40 -0400

    web scraping assignment added

---
+++ webScraper.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape(url):
+	response = requests.get(url)
+	soup = BeautifulSoup(response.content, "html.parser")
+	getName = soup.find_all("li", string = lambda text: "name:" in text.lower())
+	getPurpose = soup.find_all("li", string = lambda text: "purpose:" in text.lower())
+	companyPurpose = getPurpose[0].text[9:]
+	companyName = getName[0].text[6:]
+	return companyName + ", " + companyPurpose + "\n"
+
+if __name__ == "__main__":
+	url = "http://18.207.112.240:8000/random_company"
+	f = open("results.txt", "w")
+	f.write("Name, Purpose\n")
+	for i in range(50):
+		myString = scrape(url)
+		f.write(myString)
+	f.close()
+
+
+
+	
